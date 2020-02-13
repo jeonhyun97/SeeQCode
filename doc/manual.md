d4cf89b7286610ea401f137f50b791694e73a5d3
Hyeon Jeon <jeonhyun97@gmail.com>
2020-02-13 11:12:06 +0900

    manual markdown file rendering in the web finished

---
+++ doc/manual.md
@@ -0,0 +1,23 @@
+<!-- Sample maunal-->
+##### Video Killed the radio star
+
+[Buggles](https://www.youtube.com/watch?v=Iwuy4hHO3YQ)
+
+###### Verse
+
+I heard you on the wireless back in fifty two
+Lying awake intent at tuning in on you
+If I was young it didn't stop you coming through
+
+1. They took the **credit for your second symphony**
+2. Rewritten by machine on new technology
+3. And now I understand the problems you can see
+   
+###### Chorus
+
+I met your children
+
+- What did you tell them?
+    - *Video killed the radio star*
+    - *Video killed the radio star*
+- Pictures came and broke your heart
---
+++ src/js/design/doc.js
@@ -0,0 +1,26 @@
+
+
+function readTextFile(file) {
+    let md;
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                md = rawFile.responseText;
+            }
+        }
+    };
+    rawFile.send(null);
+    return md;
+}
+
+
+
+function initManual() {
+    
+    let html = new showdown.Converter().makeHtml(readTextFile("../doc/manual.md"));
+    document.getElementById("manualDiv").innerHTML = html;
+}
+
+initManual();
